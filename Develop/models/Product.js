7a3981780bf8367651a2d0a3d66a44e18d256f7e
brendansingerman <brendan@covr.care>
2022-12-19 18:29:03 -0700

    updated several files

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,7 +10,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model:"category",
+        key:"id"
+      }
+    }
   },
+
   {
     sequelize,
     timestamps: false,
